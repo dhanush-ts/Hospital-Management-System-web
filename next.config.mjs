17779146f3832a6ded2801583fb8599033feac7d
BarathKumarSJ-20 <231801020@rajalakshmi.edu.in>
2025-04-23 12:57:48 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     images: {
       domains: ["imgs.search.brave.com"], // Define allowed domains for images
-      domains: ["media.gettyimages.com"], // Define allowed domains for images
+      domains: ["media.gettyimages.com"], 
     },
   };
   
